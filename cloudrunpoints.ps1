8fee8f4b007687b1ab6f01c510d1fbc0a47f2311
crepmaster <ebongueandre@gmail.com>
2025-08-25 04:39:38 +0200

    ajout test-cloudrun

---
+++ cloudrunpoints.ps1
@@ -0,0 +1,7 @@
+Health            = https://health-b5eslgu2iq-ew.a.run.app
+Top-up Intent     = https://topupintent-b5eslgu2iq-ew.a.run.app
+MTN MoMo Webhook  = https://momowebhook-b5eslgu2iq-ew.a.run.app
+Orange Webhook    = https://orangewebhook-b5eslgu2iq-ew.a.run.app
+Create Hold       = https://createexchangehold-b5eslgu2iq-ew.a.run.app
+Capture Exchange  = https://exchangecapture-b5eslgu2iq-ew.a.run.app
+Cancel Exchange   = https://exchangecancel-b5eslgu2iq-ew.a.run.app
